7025175d2148c72b310e45725354263925b13284
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-03-26 11:25:56 +0200

    Create DepositStepper story

--- src/pages/ValidatorManagement/ManageValidator/DepositStatus/DepositStepper.stories.tsx
+++ src/pages/ValidatorManagement/ManageValidator/DepositStatus/DepositStepper.stories.tsx
@@ -1,21 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import DepositStatus from './DepositStatus'
+import DepositStepper from './DepositStepper'
 import { withRouter } from 'storybook-addon-react-router-v6'
 
 const meta = {
-  title: 'Manage Validator/DepositStatus',
-  component: DepositStatus,
+  title: 'Manage Validator/DepositStepper',
+  component: DepositStepper,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
   decorators:[withRouter]
-} satisfies Meta<typeof DepositStatus>
+} satisfies Meta<typeof DepositStepper>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: { activeStep: 3 },
 }
