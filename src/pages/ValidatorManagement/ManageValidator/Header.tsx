dbdd6f2faeb6f284b8c51c3792e59294bb9fe3c1
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-03-26 11:16:18 +0200

    fix(header): spacing

--- src/pages/ValidatorManagement/ManageValidator/Header.tsx
+++ src/pages/ValidatorManagement/ManageValidator/Header.tsx
@@ -3,7 +3,13 @@ import Titles from '../../../components/General/Titles'
 import { PopupIcon } from '@status-im/icons'
 const Header = () => {
   return (
-    <XStack justifyContent="space-between" alignItems="center" padding="30px">
+    <XStack
+      justifyContent="space-between"
+      alignItems="center"
+      style={{
+        padding: '12px 30px',
+      }}
+    >
       <XStack alignItems="flex-end">
         <Titles title="Manage Validator" titleSize={27} subtitle="" />
       </XStack>
