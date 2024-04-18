e9828495738dc9de228c665291278dbb0c103164
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-04-18 14:31:14 +0300

    demo for sasha one step ahead

---
+++ libs/waku-utils/examples/x.ts
@@ -0,0 +1,77 @@
+import debug from 'debug'
+// import { writeFileSync } from 'fs'
+
+import {
+  IDecoder,
+  Protocols,
+  createDecoder,
+  createEncoder,
+  createLightNode,
+  waitForRemotePeer,
+} from '@waku/sdk'
+import * as utils from '@waku/utils/bytes'
+
+import { getPairingObject, proceedHandshake } from 'waku-utils/src/waku-utils'
+import { HMACDRBG } from '@stablelib/hmac-drbg'
+import { NoiseHandshakeDecoder, NoiseSecureMessage } from '@waku/noise'
+import { randomBytes } from '@stablelib/random'
+import { wakuDnsDiscovery, enrTree } from '@waku/discovery'
+
+import { X25519DHKey } from '@waku/noise'
+import { contentTopicToPubsubTopic } from '@waku/utils'
+
+debug.enable('nimbus-gui:*,waku:noise:pairing,waku:*')
+
+const shardId = 'xxxx'
+const contentTopic = `/nimbus-gui/0.0.0/${shardId}/proto`
+
+const encoder = createEncoder({ pubsubTopic: '/waku/2/rs/1/2', contentTopic })
+
+import protobuf from 'protobufjs'
+async function main() {
+  const node = await createLightNode({
+    defaultBootstrap: false,
+    pubsubTopics: ['/waku/2/rs/1/2'],
+    shardInfo: {
+      contentTopics: [contentTopic],
+    },
+    libp2p: {
+      peerDiscovery: [
+        wakuDnsDiscovery([enrTree['SANDBOX']], {
+          lightPush: 5,
+          filter: 5,
+          store: 5,
+        }),
+      ],
+    },
+  })
+
+  await node.start()
+  await waitForRemotePeer(node, [Protocols.Filter, Protocols.LightPush])
+
+  // Create a message structure using Protobuf
+  const ChatMessage = new protobuf.Type('ChatMessage')
+    .add(new protobuf.Field('timestamp', 1, 'uint64'))
+    .add(new protobuf.Field('sender', 2, 'string'))
+    .add(new protobuf.Field('message', 3, 'string'))
+
+  // Create a new message object
+  const protoMessage = ChatMessage.create({
+    timestamp: Date.now(),
+    sender: 'Alice',
+    message: 'Hello, World!',
+  })
+
+  // Serialise the message using Protobuf
+  const serialisedMessage = ChatMessage.encode(protoMessage).finish()
+  console.log(serialisedMessage)
+  // Send the message using Light Push
+  console.log(encoder)
+  await node.lightPush.send(encoder, {
+    payload: serialisedMessage,
+  })
+}
+
+main().then(() => {
+  console.log('Message sent')
+})
